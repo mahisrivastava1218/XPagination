8d649ca4b1f3d68a03d1e47a869f73904c489be8
Mahima <priyanshisrivastava708016@gmail.com>
2025-06-23 20:56:05 +0530

    api

--- pagination/src/components/api/api.jsx
+++ pagination/src/components/api/api.jsx
@@ -1,13 +1,12 @@
- import React
- import { useState,useEffect } from "react";
- export default function Employeeapi(){
+import { useState,useEffect } from "react";
+export default function Employeeapi(){
  const [employeeData,setEmployeeData] = useState([]);
  const fetchEmployeeDetails=()=>{
          fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
          .then(response=>{
           if(!response.ok){
            throw new Error("failed to fetch data") ;
-           
+
           }
           return response.json();
          })
